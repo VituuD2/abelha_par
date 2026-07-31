c76ea91cc00b5d4858167f3b7c7e287fe602106e
vituud2 <overroot0@gmail.com>
2026-07-31 16:52:16 -0300

    feat: initialize application layout with global styles, Inter font, and persistent sidebar navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <Sidebar />
           
           {/* Main Content */}
-          <main className="w-full lg:!pl-[260px] pb-20 lg:pb-0 min-h-screen flex flex-col">
+          <main className="w-full lg:!pl-[300px] pb-20 lg:pb-0 min-h-screen flex flex-col">
             <div className="w-full max-w-6xl mx-auto px-6 sm:px-10 lg:px-12 py-10 lg:py-16">
               {children}
             </div>
